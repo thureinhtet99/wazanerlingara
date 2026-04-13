f0ea7ece8069bf37e52c3c5b12f82e2288e2ca24
thureinhtet99 <ignite25399@gmail.com>
2026-04-14 00:11:22 +0700

    Refactor image imports and update category card component
    - Replaced image imports in dummy-data.ts with SVG references from icons.ts.
    - Updated category card component to use Image from react-native instead of SvgAsset.
    - Changed the type of image in CategoryCardType to ImageSourcePropType for better compatibility.
    - Removed unused imports and cleaned up the icons.ts file.
    - Adjusted game configuration to remove commented-out code.

--- app/categories.tsx
+++ app/categories.tsx
@@ -5,17 +5,22 @@ import { Button } from "@/components/ui/button";
 import { CONFIG } from "@/constants/config";
 import { CATEGORIES } from "@/constants/dummy-data";
 import CategoryCard from "@/features/categories/components/category-card";
+import { useAudioSettings } from "@/hooks/use-audio-settings";
+import { useGameConfig } from "@/hooks/use-game-config";
 import { CategoryType } from "@/types/index.types";
 import { router } from "expo-router";
 import { useState } from "react";
 import { ScrollView, View } from "react-native";
+import Loading from "./loading";
 
 export default function ChooseCategories() {
-  // const { config, updateGameConfig } = useGameConfigStore();
-  const [category, setCategory] = useState<CategoryType | undefined>();
+  const { config, loading, updateGameConfig } = useGameConfig();
+  const [category, setCategory] = useState<CategoryType>(config.category);
+  const { playClickSound } = useAudioSettings();
 
   const handleClick = (type: CategoryType) => {
     setCategory(type);
+    playClickSound();
   };
 
   const handleGameForward = () => {
@@ -24,15 +29,13 @@ export default function ChooseCategories() {
     const selectedCategory = CATEGORIES.find((item) => item.type === category);
     if (!selectedCategory) return;
 
-    // if (config) {
-    //   updateGameConfig({
-    //     category: {
-    //       id: selectedCategory.type,
-    //       name: selectedCategory.title,
-    //     },
-    //   });
+    updateGameConfig({
+      category: selectedCategory.type,
+    });
     router.push(CONFIG.GAME_SETTING);
   };
+
+  if (loading) return <Loading />;
 
   return (
     <ThemedView className="flex-1">
@@ -42,14 +45,18 @@ export default function ChooseCategories() {
         <ThemedText type="title">ရွေးချယ်စရာများ</ThemedText>
       </View>
 
-      <ScrollView className="flex-1" contentContainerClassName="pb-2">
-        <View className="flex-row flex-wrap">
+      <ScrollView
+        className="flex-1"
+        contentContainerClassName="pb-10"
+        showsVerticalScrollIndicator={false}
+      >
+        <View className="flex-row flex-wrap justify-between gap-4">
           {CATEGORIES.map((item) => (
-            <View key={item.type} className="w-1/2 px-2 mb-4">
+            <View key={item.type} className="w-[48%]">
               <CategoryCard
                 category={item}
                 isSelected={item.type === category}
-                onSelect={handleClick}
+                handleClick={handleClick}
               />
             </View>
           ))}
